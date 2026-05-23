b1973fc64148fc7fde077b972ca3dbe372c31774
oscartroper <oscarbarajasbartres@gmail.com>
2026-05-22 19:57:19 -0600

    Create cocina4.js
    practica 4, promesas y rejects

---
+++ Practica4/cocina4.js
@@ -0,0 +1,158 @@
+// Cocina Productos
+// Array = catalogo
+// Objeto = cada producto { }
+// Propiedades = id, nombre, precio, categoria
+// Find = buscar un producto específico por su id
+// Filter = filtrar productos por precio o categoría
+// Promesa = representa una operación que tarda tiempo
+// Resolve = producto preparado correctamente
+// Reject = error en cocina o falta ingrediente
+// setTimeout = simula tiempo de preparación
+// Math.random = genera eventos aleatorios
+
+let catalogo = [
+
+    {
+        id: 1,
+        nombre: "Capuchino",
+        precio: 55,
+        categoria: "Bebida caliente",
+    },
+
+    {
+        id: 2,
+        nombre: "Latte",
+        precio: 60,
+        categoria: "Bebida caliente",
+    },
+
+    {
+        id: 3,
+        nombre: "Americano",
+        precio: 40,
+        categoria: "Bebida caliente",
+    },
+
+    {
+        id: 4,
+        nombre: "Frappé Moka",
+        precio: 75,
+        categoria: "Bebida fría",
+    },
+
+    {
+        id: 5,
+        nombre: "Cheesecake",
+        precio: 65,
+        categoria: "Postre",
+    },
+
+    {
+        id: 6,
+        nombre: "Frapuchino de Caramelo",
+        precio: 70,
+        categoria: "Bebida fría",
+    },
+
+    {
+        id: 7,
+        nombre: "Té Verde",
+        precio: 45,
+        categoria: "Bebida caliente",
+    },
+
+    {
+        id: 8,
+        nombre: "Galleta de chocolate",
+        precio: 50,
+        categoria: "Postre",
+    },
+
+    {
+        id: 9,
+        nombre: "Refresher de Frutas",
+        precio: 55,
+        categoria: "Bebida fría",
+    },
+
+    {
+        id: 10,
+        nombre: "Croissant",
+        precio: 30,
+        categoria: "Postre",
+    }
+
+];
+
+
+
+// PROMESA DE COCINA
+
+function prepararProducto(idProducto){
+
+return new Promise((resolve,reject)=>{
+
+let producto = catalogo.find(
+producto=>producto.id===idProducto
+);
+
+
+console.log(`
+Preparando ${producto.nombre}...
+`);
+
+
+// Simular tiempo
+
+setTimeout(()=>{
+
+
+// Simular ingredientes aleatorios
+
+let ingredienteDisponible = Math.random() > 0.5;
+
+
+// Si hay ingrediente
+
+if(ingredienteDisponible){
+
+resolve(`
+${producto.nombre} listo
+`);
+
+}
+
+// Si faltan ingredientes
+
+else{
+
+reject(`
+Error en cocina:
+No hay ingredientes para ${producto.nombre}
+`);
+
+}
+
+},3000);
+
+});
+
+}
+
+
+
+// EJECUCIÓN
+
+prepararProducto(2)
+
+.then(resultado=>{
+
+console.log(resultado);
+
+})
+
+.catch(error=>{
+
+console.log(error);
+
+});
