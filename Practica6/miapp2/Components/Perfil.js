51592e6e0539ca0cf39fd4ef116859ebb19e1ac7
oscartroper <oscarbarajasbartres@gmail.com>
2026-06-12 20:00:45 -0600

    Practica 8 Parte 1
    Primera parte de la practica utilizando los Stylesheets

--- Practica6/miapp2/Components/Perfil.js
+++ Practica6/miapp2/Components/Perfil.js
@@ -2,29 +2,53 @@
  y se muestran en el componente hijo (Perfil.js) a través de las props. 
  En este caso, se le pasan el nombre, carrera, materia y cuatrimestre del estudiante.*/
 
-import{View, Text, Button} from 'react-native'
+import{View, Text, Button, StyleSheet} from 'react-native'
 import React, {useState} from 'react'
 
 export const Perfil = ({nombre, carrera, materia, cuatri}) => {
 
     const [mostrar, setMostrar] = useState(false);
     return (
-        <View>
-            <Text>{nombre}</Text>
+        <View style={styles.tarjeta}>
+            <Text style={styles.nombre}>{nombre}</Text>
 
             {/*Renderizado condicional, es decir, se muestra un botón para mostrar u ocultar los datos del estudiante.*/}
             { mostrar && 
                 <>
 
-                    <Text>{carrera}</Text>
-                    <Text>{materia}</Text>
-                    <Text>{cuatri}</Text>
+                    <Text style={styles.carrera}>{carrera}</Text>
+                    <Text style={styles.otroTexto}>{materia}</Text>
+                    <Text style={styles.otroTexto}>{cuatri}</Text>
                 </>
             }
             <Button title="Mostrar perfil" onPress={() => setMostrar(!mostrar)} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    nombre: {
+        fontSize: 24,
+        fontWeight: 600,
+        textTransform: 'uppercase',
+    },
+    carrera: {
+        fontSize: 18,
+        color: 'blue',
+        fontFamily: 'Roboto',
+    },
+    otroTexto: {
+        fontSize: 12,
+        fontFamily: 'Courier',
+        fontStyle: 'italic',
+    },
+    tarjeta: {
+        borderWidth: 2,
+        padding: 25,
+        margin: 15,
+    },
+})
+
 
 
 /*Pefil usando props, es decir, se le pasan los datos desde el componente padre
