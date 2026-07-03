6fac597ef194883fc8116fd6843c0291f033835f
oscartroper <oscarbarajasbartres@gmail.com>
2026-07-02 22:24:00 -0600

    Practica ActivityIndicator, KeyboardAvoidingView

--- Practica6/miapp2/screens/ActivityIndicator.js
+++ Practica6/miapp2/screens/ActivityIndicator.js
@@ -1,26 +1,92 @@
-/*zona1: importaciones de componentes y archivos*/
+/*Zona 1: Importaciones de componentes y archivos */
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,} from 'react-native';
-/* Zona2: Main - Hogar de los componentes */
-export default function ActivityIndicator() {
+import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+
+/*Zona 2: Main - Hogar de los componentes */
+export default function ActivityIndicatorScreen() {
+  const [cargando, setCargando] = useState(false);
+
+  const handleSubmit = () => {
+    Keyboard.dismiss();
+    setCargando(true);
+    setTimeout(() => {
+      setCargando(false);
+    }, 2000);
+  };
+
   return (
-    <View style={styles.container}>
-        <Text>Aqui va la practica de Ana</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      style={styles.screen}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+      enabled={true}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text style={styles.header}>Header</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+          />
+          {cargando && (
+            <ActivityIndicator
+              animating={true}
+              size="large"
+              color="green"
+              style={styles.spinner}
+            />
+          )}
+          <View style={styles.buttonWrapper}>
+            <Button title="Submit" onPress={handleSubmit} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
-/* Zona3: Estilos y Posicionamientos */
+/*Zona 3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    flexDirection: 'row',
   },
 
-  tarjetaRoja:{backgroundColor: '#ff6b6b'},
-  tarjetaVerde:{backgroundColor: '#6bcb77'},
+  container: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+
+  header: {
+    fontSize: 36,
+    marginBottom: 48,
+  },
+
+  input: {
+    height: 40,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+    marginBottom: 36,
+  },
+
+  spinner: {
+    marginVertical: 12,
+  },
+
+  buttonWrapper: {
+    backgroundColor: 'white',
+    marginTop: 12,
+  },
 });
